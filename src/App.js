47544ea489601e75cd2d70b1da4f8e5ef483c7c6
Matt Salmons <mattcsalmons@gmail.com>
2021-11-08 10:59:31 -0700

    adds header component, links to home / shop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -10,12 +11,11 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/shop" component={ShopPage} />
-        </Switch>
-      </Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
