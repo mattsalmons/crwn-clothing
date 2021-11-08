2b9fa2a8ad18453f5b12eb41084f695d9da9de11
Matt Salmons <mattcsalmons@gmail.com>
2021-11-08 10:42:30 -0700

    adds shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,22 +4,29 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+
+  const handleClick = () => {
+    history.push(`${match.url}${linkUrl}`);
+  }
+
+  return (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-      />
-      <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={handleClick}
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        />
+        <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default withRouter(MenuItem);
