cff7f03854f5d22044628fdc0a644d3a40ab7889
Matt Salmons <mattcsalmons@gmail.com>
2021-12-27 07:29:18 -0700

    added to cart items appear in dropdown and total show in bag icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -17,7 +17,7 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems} }) => ({
+const mapStateToProps = ({ cart: { cartItems } }) => ({
   cartItems
 });
 
